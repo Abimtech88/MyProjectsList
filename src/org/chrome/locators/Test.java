bcde4afcab99d4fd9055a350109e7bd46010c5ed
Abimtech88 <abimtech88@gmail.com>
2020-04-03 01:04:43 +0530

    My code

--- src/org/chrome/locators/Test.java
+++ src/org/chrome/locators/Test.java
@@ -6,12 +6,12 @@ public static void main(String[] args) {
 	int i=10 + + 11 - - 12 ;
 	//+ + 13- -14 + + 15;
 	System.out.println(i);
-/*	String s="xyz";
+	String s="xyz";
 	s.toUpperCase();
 	String y = s.replace('Y', 'y');
 	y=y+"abc";
 	System.out.println(y);
-	*/
+	
 	/*int []A = new int[8];
 	int i=5;
 	for(i=-1;i<A.length-1;)
